14422d5a7ff074eea8b72797ac7e34f2dd8d686a
7Soldier <reg.fm4@gmail.com>
2022-05-17 10:47:01 +0300

    begonia: Fix typo

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
-# Goog Photos Unlimited
+# Google Photos Unlimited
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
